4b7e64654791ba06ab6fce7c12c812ac3e6bf79e
wkosenkow <vkosenkov@gmail.com>
2026-05-27 18:33:38 +0200

    Add myLocations array with Berlin landmarks

--- src/app.js
+++ src/app.js
@@ -16,3 +16,35 @@ Leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '&copy; <a href="https://google.com" target="_blank">Google</a>',
 }).addTo(map);
+
+const myLocations = [
+  {
+    name: 'WBS CODING SCHOOL',
+    location: [52.457131, 13.54007],
+    description: 'The best coding school in the world'
+  },
+  {
+    name: 'Alexanderplatz',
+    location: [52.521918, 13.413215],
+    description: 'The most famous square in Berlin'
+  },
+  {
+    name: 'Brandenburg Gate',
+    location: [52.516275, 13.377704],
+    description: 'The most famous gate in Berlin'
+  },
+  {
+    name: 'Berlin Wall',
+    location: [52.507541, 13.39032],
+    description: 'The most famous wall in Berlin'
+  }
+];
+// Add markers to the map with a popup
+myLocations.forEach(location => {
+  Leaflet.marker(location.location, { icon: markerIcon })
+    .bindPopup(location.description)
+    .addTo(map);
+});
+// Set the view to the bounds of all markers
+const bounds = Leaflet.latLngBounds(myLocations.map(location => location.location));
+map.fitBounds(bounds);
